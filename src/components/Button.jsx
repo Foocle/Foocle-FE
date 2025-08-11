eb3cdf9f57f3e86d022d411b26473983c34e0e01
hyeryun You <152360502+hyeryunYou@users.noreply.github.com>
2025-08-11 23:48:51 +0900

    feature: [#3] 버튼 컴포넌트 구현

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,2 +1,28 @@
-// 버튼 컴포넌트
-// 버튼 안에 텍스트 {text}로 받아서 컴포넌트로 쉽게 활용
+import styled from "styled-components";
+
+const FooterBtn = styled.button`
+    width: 100%;
+    height: ${(props) => props.height};
+    text-align: center;
+    font-family: "Pretendard-SemiBold";
+    font-size: ${(props) => props.fontSize};
+    color: ${(props) => (props.reverse == "true" ? "#FFFFFF" : "#FF9B4A")};
+    background-color: ${(props) =>
+        props.reverse == "true" ? "#FF7300" : "#FFBF8A8C"};
+    border: none;
+    border-radius: 0.9375rem;
+    cursor: pointer;
+`;
+
+export const Button = ({ text, onClick, reverse, height, fontSize }) => {
+    return (
+        <FooterBtn
+            onClick={onClick}
+            reverse={reverse ? "true" : "false"}
+            height={height ? height : "4.375rem"}
+            fontSize={fontSize ? fontSize : "1.5rem"}
+        >
+            {text}
+        </FooterBtn>
+    );
+};
