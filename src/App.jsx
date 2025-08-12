677870d9b17981d85b23e1da73a879e645f731f5
hyeryunYou <hyeryun1108@naver.com>
2025-08-13 04:37:21 +0900

    feature: 시작 페이지, 온보딩 페이지, 로그인 시작 페이지 경로추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
-import { Start } from './pages/Start';
+import LoginStart from './pages/Join/LoginStart';
 import Onboarding from './pages/Onboarding/Onboarding';
+import Start from './pages/Start';
 import InputShopInfo from './pages/Curation/InputShopInfo';
 import MainLayout from './layout/MainLayout';
 import Test from './pages/Curation/Test';
@@ -8,7 +9,9 @@ import ImageUpload from './pages/Curation/ImageUpload';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Onboarding />} />
+      <Route path="/loginstart" element={<LoginStart />} />
+      <Route path="/onboarding" element={<Onboarding />} />
+      <Route path="/" element={<Start />} />
 
       {/* MainLayout 상속받는 그룹*/}
       <Route element={<MainLayout />}>
