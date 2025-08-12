de8ac060ad2a98fa5b3b2d00c8a7a9ead1131c57
hyeryunYou <hyeryun1108@naver.com>
2025-08-13 05:00:10 +0900

    refactor: 온보딩 페이지 반응형으로 수정

--- src/pages/Onboarding/Onboarding.jsx
+++ src/pages/Onboarding/Onboarding.jsx
@@ -56,7 +56,7 @@ const Onboarding = () => {
 const OnboardingWrapper = styled.div`
     width: 100%;
     max-width: 600px;
-    flex-grow: 1; // 남은 공간 모두 차지
+    flex-grow: 1;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
@@ -73,16 +73,16 @@ const SkipHeader = styled.header`
     border-bottom: 1px solid #D0D0D0;
 `;
 const SkipButton = styled.button`
-    width: 143px;
-    height: 60px;
+    width: 9rem;
+    height: 3.75rem;
     border: none;
     border-radius: 10px;
     background-color: #F0F0F0;
     cursor: pointer;
 
     p {
-        font-size: 24px;
-        font-family: Pretendard-Medium;
+        font-size: 1.5rem;
+        font-family: "Pretendard-Medium";
         color: #4D4D4D;
     }
 `;
@@ -91,7 +91,7 @@ const Body = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    margin: 97px 0 40px 0;
+    margin: 6rem 0 2.5rem 0;
 `
 const Image = styled.img`
     width: 100%;
@@ -103,8 +103,8 @@ const Card = styled.main`
     align-items: center;
     justify-content: space-between;
     position: relative;
-    padding: 24px 2rem 2rem 2rem;
-    border-radius: 40px 40px 0 0;
+    padding: 1.5rem 2rem 2rem 2rem;
+    border-radius: ${R}rem ${R}rem 0 0;
     overflow: visible;
 
     &::before {
@@ -123,40 +123,40 @@ const Card = styled.main`
 `;
 const DotsWrapper = styled.div`
     display: flex;
-    gap: 8px;
+    gap: 0.5rem;
     justify-content: center;
     align-items: center;
 `;
 const Dots = styled.span`
-    width: 70px;
-    height: 4px;
+    width: 4.375rem;
+    height: 0.25rem;
     display: inline-block;
-    border-radius: 5px;
+    border-radius: 0.3125rem;
     background: ${({ $active }) => ($active ? "#FF7300" : "#FFBF8A8C")};
     transition: background 0.05s ease;
 `;
 const CardTitleSection = styled.div`
     width: 100%;
-    height: 172px;
-    padding: 30px 0;
+    height: 10.75rem;
+    padding: 1.875rem 0;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     text-align: center;
 `;
 const CardTitle = styled.p`
-    font-size: 42px;
+    font-size: 2.6rem;
     font-family: "Pretendard-SemiBold";
     color: #222222;
-    line-height: 50px;
+    line-height: 3.1rem;
 `;
 
 const CardDesc = styled.p`
     width: 100%;
-    height: 80px;
-    font-size: 24px;
+    height: 5rem;
+    font-size: 1.5rem;
     font-family: "Pretendard-Medium";
-    line-height: 40px;
+    line-height: 2.5rem;
     color: #4D4D4D;
     white-space: pre-line; /* \n을 줄바꿈으로 렌더링 */
 `;
