dfc1d85f6c06dbbc916221d78386165ed62586f8
hyeryun You <152360502+hyeryunYou@users.noreply.github.com>
2025-08-19 16:43:24 +0900

    api: #27 로그아웃 구현

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -5,8 +5,9 @@ const useAuthStore = create(
   persist(
     (set) => ({
       token: null, // Access Token
-      setToken: (token) => set({ token }),
-      clearToken: () => set({ token: null }),
+      isLoggedIn: false, // 로그인 상태
+      setToken: (token) => set({ token, isLoggedIn: !!token }), // 토큰이 있을 때 true로 설정
+      clearToken: () => set({ token: null, isLoggedIn: false }), // 로그아웃 시 false로 설정
     }),
     {
       name: 'auth-storage',
